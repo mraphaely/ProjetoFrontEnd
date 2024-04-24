c2782e6501c6b43f5062989c3306a9ecda3f8cca
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-04-24 18:33:08 -0300

    Delete src directory

--- src/Modal/ButtonModal.jsx
+++
@@ -1,9 +0,0 @@
-
-// eslint-disable-next-line react/prop-types
-const ButtonModal = ({modal, setModal}) => {
-    return(
-        <button onClick={() => setModal(true)}>Abrir Modal</button>
-    )
-}
-
-export default ButtonModal; 
